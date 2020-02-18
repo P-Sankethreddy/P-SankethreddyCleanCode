43f4fc46b33f25bfba30fb84a1564aa834aa1ceb
P-Sankethreddy <51527304+P-Sankethreddy@users.noreply.github.com>
2020-02-18 19:52:24 +0530

    EPAM CleanCode 1st commit

---
+++ src/main/java/EPAM_CLEANCODE/EPAM_CLEANCODE/App.java
@@ -0,0 +1,46 @@
+package EPAM_CLEANCODE.EPAM_CLEANCODE;
+
+import java.io.IOException;
+
+import java.util.*;
+import java.io.*;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args ) throws UnknownHostException, IOException
+    {
+    	int choice;
+        Scanner sc=new Scanner(System.in);
+    	Socket s= new Socket( "localhost",8888 );
+    	Scanner sc1=new Scanner(s.getInputStream());
+    	PrintStream p=new PrintStream(s.getOutputStream());
+    	
+    
+    		System.out.println("Enter any choice");
+    	System.out.println("1.SimpleInterest\n2.CompoundInterest\n3.HouseCostEstimation");
+    	choice=sc.nextInt();
+    	p.println(choice);
+    	
+    
+    	
+    	 
+    	BigDecimal Result=sc1.nextBigDecimal();
+    	if(Result.equals(new BigDecimal(Integer.MIN_VALUE)))
+    	{
+    		System.err.println("Invalid Choice");
+    	}
+    	else
+    		
+    	{
+    		
+    		
+    	 	System.out.println(Result);
+    	}
+    	
+    }
+}
